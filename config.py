32fb092dfb5ece496fad7857f683c20edf72ba93
Prakhar Awasthi <32389588+prakhar199@users.noreply.github.com>
2020-12-28 10:56:09 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,25 @@
+import pprint
+
+class Config():
+
+    def __init__(self, **kwargs):
+
+        self.data_path = '/content/drive/MyDrive/fcsn_tvsum.h5'
+        self.save_dir = '/content/save_dir'
+        self.score_dir = '/content/score_dir'
+
+        self.n_epochs = 5
+        self.batch_size = 5
+
+        for a,b in kwargs.items():
+            setattr(self,a,b)
+
+    def __repr__(self):
+
+        config_str = 'Configurations\n' + pprint.pformat(self.__dict__)
+
+        return config_str
+
+if __name__ == '__main__':
+    config = Config()
+    print(config)
